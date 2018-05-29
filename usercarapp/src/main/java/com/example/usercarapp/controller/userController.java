e81122fca88986bd725844a08426b0af88da17af
sss <sss@sss>
2018-05-29 18:22:27 +0200

    paquete controller

--- usercarapp/src/main/java/com/example/usercarapp/controller/userController.java
+++ usercarapp/src/main/java/com/example/usercarapp/controller/userController.java
@@ -1,5 +1,21 @@
 package com.example.usercarapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
 public class userController {
 
+	UserService carService;
+	
+	UserMapper carMapper;
+	
+	@RequestMapping(value = "/{id}"), method = RequestMethod.)
+	public UserDTO findByID(@RequestParam(defaultValue = "0", required = false) Integer id) {
+		
+		final Optional<User> user = 
+	}
+	
 }
