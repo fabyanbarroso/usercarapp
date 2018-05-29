5631163273af4525ecf9de73eb4e4a46728a48d4
Julito <Julito@localhost>
2018-05-29 18:01:51 +0200

    Model Car

--- usercarapp/src/main/java/com/example/usercarapp/model/Car.java
+++ usercarapp/src/main/java/com/example/usercarapp/model/Car.java
@@ -1,5 +1,25 @@
 package com.example.usercarapp.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
 public class Car {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private String idCar;
+	
+	@Column(nullable = false)
+	private String model;
+	
 
 }
