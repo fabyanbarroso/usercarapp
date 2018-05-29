2ed8ff66eb4ee64d5ae29a03b78cd4fce3df6b4f
Fabian <Fabian@10.11.99.159>
2018-05-29 17:53:37 +0200

    User Model

--- usercarapp/src/main/java/com/example/usercarapp/model/User.java
+++ usercarapp/src/main/java/com/example/usercarapp/model/User.java
@@ -1,5 +1,36 @@
 package com.example.usercarapp.model;
 
-public class User {
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	private int idUser;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column()
+	private String password;
+	
+	@OneToMany(fetch = FetchType.LAZY)
+	Set<Car> cars;
 }
