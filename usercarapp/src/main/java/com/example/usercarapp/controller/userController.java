5deb8f90cd4d6a62324cf5030e0ffbbae46ddd97
sss <sss@sss>
2018-05-29 18:39:11 +0200

    controller+

--- usercarapp/src/main/java/com/example/usercarapp/controller/userController.java
+++ usercarapp/src/main/java/com/example/usercarapp/controller/userController.java
@@ -1,9 +1,17 @@
 package com.example.usercarapp.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.usercarapp.dto.UserDTO;
+import com.example.usercarapp.model.User;
+import com.example.usercarapp.service.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -29,6 +37,9 @@ public class userController {
 		final Optional<User> user = userService.findById(id);
 		return userMapper.modelToDto(user.get());
 	}
+	
+	
+	
 	
 	
 	
