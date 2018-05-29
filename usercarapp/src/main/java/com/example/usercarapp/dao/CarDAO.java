72ec6be1560c7d4d9dcbf7220df0697631858b7f
Fabian <Fabian@10.11.99.159>
2018-05-29 18:16:14 +0200

    DAOS

--- usercarapp/src/main/java/com/example/usercarapp/dao/CarDAO.java
+++ usercarapp/src/main/java/com/example/usercarapp/dao/CarDAO.java
@@ -4,17 +4,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import com.example.usercarapp.model.Car;
-import com.example.usercarapp.model.User;
 
 @Repository
-public interface CarDAO extends PagingAndSortingRepository<User,Integer>{
+public interface CarDAO extends PagingAndSortingRepository<Car,Integer>{
 
 	@Query(value = "select c "
 			+ "from Car as c "
 			+ "where c.idCar = ?1")
-	public Car findByIdUser(@Param(value = "idCar")String idCar);
+	public Car findByIdCar(@Param(value = "idCar")String idCar);
+	
+	@Query(value = "SELECT * "
+			+ "FROM CAR "
+			+ "WHERE model = ?1", nativeQuery=true)
+	public Car findByModelCar(@Param(value = "idCar")String idCar);
 	
 	
 }
