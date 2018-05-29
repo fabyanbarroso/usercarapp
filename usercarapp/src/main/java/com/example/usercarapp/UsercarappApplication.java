0ac69183a7164d1d31ec11d9fb7a3c53dee0c06e
Fabian <Fabian@10.11.99.159>
2018-05-29 17:38:38 +0200

    adsad

---
+++ usercarapp/src/main/java/com/example/usercarapp/UsercarappApplication.java
@@ -0,0 +1,13 @@
+package com.example.usercarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsercarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsercarappApplication.class, args);
+	}
+}
+
